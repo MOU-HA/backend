6346091b00b58c8ad6f861998b4a04f7ea348293
HAFFAD MOURAD <haffadmourad86@gmail.com>
2018-05-27 00:37:26 +0100

    Mehtode saveOrUpdate is working

---
+++ src/main/java/com/youtube/demo/serviceImpl/UserServiceImpl.java
@@ -0,0 +1,23 @@
+package com.youtube.demo.serviceImpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.youtube.demo.dao.UserRepository;
+import com.youtube.demo.model.User;
+import com.youtube.demo.service.UserService;
+
+
+@Service
+public class UserServiceImpl implements UserService {
+	
+	@Autowired
+	protected UserRepository userRepository;
+
+	@Override
+	public User save(User user) {
+		return userRepository.save(user);
+	}
+	
+
+}
