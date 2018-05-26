c558498ec1dd2ec02684f2f9e83ea2ef01631be4
HAFFAD MOURAD <haffadmourad86@gmail.com>
2018-05-27 00:36:45 +0100

    Methode SaveOrUpdate is working

--- src/main/java/com/youtube/demo/model/User.java
+++ src/main/java/com/youtube/demo/model/User.java
@@ -26,4 +26,40 @@ public class User extends ParentEntity {
 	 private String phone;	 
 	 @Column(name = "address", nullable = true, length = 150)
 	 private String address;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getSecondName() {
+		return secondName;
+	}
+	public void setSecondName(String secondName) {
+		this.secondName = secondName;
+	}
+	public String getFirstSurname() {
+		return firstSurname;
+	}
+	public void setFirstSurname(String firstSurname) {
+		this.firstSurname = firstSurname;
+	}
+	public String getSecondSurname() {
+		return secondSurname;
+	}
+	public void setSecondSurname(String secondSurname) {
+		this.secondSurname = secondSurname;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}	 
 }
